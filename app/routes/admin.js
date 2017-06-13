43860b907622abd799e4d92d3b9538050567c34c
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-13 14:55:35 -0300

    add validation errors

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,11 +6,27 @@ module.exports = function (application) {
 
   application.post('/noticias/salvar', function(req, res){
     var noticia = req.body;
+   
+    req.assert('titulo', 'Titulo obrigatorio').notEmpty();
+    req.assert('resumo', 'Resumo obrigatorio').notEmpty();
+    req.assert('resumo', 'Resumo deve conter o minimo de 10 e maximo de 100 caracteres').len(10,100);
+    req.assert('autor', 'Autor obrigatorio').notEmpty();
+    req.assert('data_noticia', 'Data obrigatorio').notEmpty().isDate({format: 'YYYY-MM-DD'});
+    req.assert('noticia', 'Noticia obrigatorio').notEmpty();
+    
+    var errors = req.validationErrors();
+
+    console.log(errors);
+    
+    if(errors){
+        res.render("admin/form_add_noticia");
+      return;
+    }
 
     var connection = application.config.dbConnection();
-    var noticiasModel = new application.app.models.NoticiasDAO(connection);
+    var noticiasDAO = new application.app.models.NoticiasDAO(connection);
 
-    noticiasModel.salvarNoticia(function(error, result){
+    noticiasDAO.salvarNoticia(noticia, function(error, result){
       res.redirect("/noticias");
     });
 
