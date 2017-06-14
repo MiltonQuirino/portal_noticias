b0510ac0fffa9808ce8ed910db513b32b30f6334
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-14 10:23:58 -0300

    load news dynamically

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,13 @@
 module.exports.index = function(application, req, res){
-   res.render('home/index');
+
+  var connection =  application.config.dbConnection();
+  var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+  var quantidade = 5 ;
+
+  noticiasModel.getUltimasNoticias(quantidade, function(error, result){
+    console.log(result);
+    res.render('home/index',{noticias:result});
+  });
+   
 }
